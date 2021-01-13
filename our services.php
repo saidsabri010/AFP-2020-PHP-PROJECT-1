10d055f1292fc0bbf4b34807e8bee15188307c58
Rasul06 <kochkarov.001.rasul@gmail.com>
2021-01-13 02:31:16 +0100

    our services.php
    i create our services php file

---
+++ our services.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<style type="text/css">
+body,td,th {
+	color: #000000;
+}
+body {
+	background-color: #D9D1D1;
+	margin-left: 100px;
+	background-image: url(file:///C|/xampp/htdocs/AFP-2020-PHP-PROJECT/images/download.jpg);
+	background-repeat: no-repeat;
+}
+a:link {
+	color: #E91518;
+}
+</style>
+</head>
+
+<body bgcolor="#D02D30">
+<p>&nbsp;</p>
+<h1>
+  <p>       <em><strong>  </strong></em>Welcome to our web page. </p></h1>
+<h2>If you are new here then You definitely need to know what we do here. </h2>
+<h2>And what we do here is:</h2>
+<h3> WE GIVE YOU A CHANCE TO FIND PEOPLE LIKE YOU. </h3>
+<h3>WE GIVE YOU A CHANCE TO FIND ANSWERS FOR YOUR QUESTIONS.</h3>
+<h3>WE GIVE YOU AN ENJOYABLE MOMENT.</h3>
+<h4> I know you are thinking what We can do here. </h4>
+<h3>This is a one any usual web pages where you can create posts.</h3>
+<h3> You can ask questions. You can understand any other peoples' ideas. You can have a chat. You can share your stories. You can share your best time photos, Highlights from your life <em>AND STILL CAN BE PRIVATE FOR PUBLIC</em>.</h3>
+
+<p>&nbsp;</p>
+	 <h2>ARE YOU READY</h2>
+	 <h1>Then click to -&gt; Create Your Post &lt;- </h1>
+</body>
+</html>
