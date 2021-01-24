bf6c7f558cfdd25d9b949534a33a8515d0efc787
Rasul06 <kochkarov.001.rasul@gmail.com>
2021-01-24 22:55:27 +0100

    our services style finished

--- blog/our_services.php
+++ blog/our_services.php
@@ -8,22 +8,23 @@
 </head>
 
 <body>
+	<a href="'blog/images/our_services.jpg">
 <p>&nbsp;</p>
 <h1>
   <p>       <em><strong>  </strong></em>Welcome to our web page. </p></h1>
-<h2>If you are new here then You definitely need to know what we do here. </h2>
-<h2>And what we do here is:</h2>
-<h3> WE GIVE YOU A CHANCE TO FIND PEOPLE LIKE YOU. </h3>
-<h3>WE GIVE YOU A CHANCE TO FIND ANSWERS FOR YOUR QUESTIONS.</h3>
-<h3>WE GIVE YOU AN ENJOYABLE MOMENT.</h3>
-<h4> I know you are thinking what We can do here. </h4>
-<h3>This is a one any usual web pages where you can create posts.</h3>
-<h3> You can ask questions. You can understand any other peoples' ideas. You can have a chat. You can share your stories. You can share your best time photos, Highlights from your life <em>AND STILL CAN BE PRIVATE FOR PUBLIC</em>.</h3>
+<h2 style="color:blue">If you are new here then You definitely need to know what we do here. </h2>
+<h2 style="color:blue">And what we do here is:</h2>
+<h3 style="color:red"> WE GIVE YOU A CHANCE TO FIND PEOPLE LIKE YOU. </h3>
+<h3 style="color:red" >WE GIVE YOU A CHANCE TO FIND ANSWERS FOR YOUR QUESTIONS.</h3>
+<h3 style="color:red">WE GIVE YOU AN ENJOYABLE MOMENT.</h3>
+<h4 style="color:yellow"> I know you are thinking what We can do here. </h4>
+<h3 style="color:red">This is a one any usual web pages where you can create posts.</h3>
+<h3 style="color:green"> You can ask questions. You can understand any other peoples' ideas. You can have a chat. You can share your stories. You can share your best time photos, Highlights from your life <em>AND STILL CAN BE PRIVATE FOR PUBLIC</em>.</h3>
 
-
+<p><h2>ARE YOU READY</h2></p>
 <p>You are not a user ! <a href="register.php"><b> Register Here</b> </a> </p>
 <p>Already a user ! <a href="login.php"><b> Login</b> </a> </p>
- <h2>ARE YOU READY</h2>
- <h1>Then click to -&gt; Create Your Post &lt;- </h1>
+ 
+ 
 </body>
 </html>
